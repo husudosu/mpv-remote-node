1f803711e98a7b0e1673875a84bb0ab91b1673ae
Ferenc Nánási <husudosu94@gmail.com>
2021-07-17 14:27:26 +0200

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-TODO
+# MPV-Remote plugin
+
+This plugin needed for MPV-Remote Android application to work.
+
+## Installation
+
+Required dependencies:
+
+- MPV
+- Node.JS with NPM installed on your PC.
+- youtube-dl for playing youtube videos,
+- MPV Remote android application
+
+This plugin & app currently only a development version, so not available on NPM yet.
+
+To install do this:
+
+```bash
+npm install -g git+https://github.com/husudosu/mpv-remote-node.git # Linux: use sudo if needed
+# Run MPV remote and follow instructions
+mpv-remote
+```
+
+## How to run MPV
+
+If you don't want MPV close after playback finished use --idle flag
+
+```
+mpv --idle
+```
+
+## Port
+
+The MPV remote plugin runs on port 8000 by default.
+
+## Supported systems
+
+Tested on Linux, Windows 10
+
+## Limitations
+
+- Currently only one instance of MPV supported,
+
+## TODO
+
+- Put the project into NPM registry,
+- Provide easier web port changing method,
+- Tidy up the code,
+- Better README
--- bin.js
+++ bin.js
@@ -8,33 +8,51 @@ const path = require("path");
 const os = require("os");
 const fs = require("fs");
 
-const target = path.join(__dirname, 'remote.socketio.js');
-const link = path.join(getScriptFolder(), 'mpvremote', 'remote.socketio.js');
+const pluginDir = path.join(__dirname, "mpvremote");
+const pluginTarget = path.join(getScriptFolder(), "mpvremote");
+
+const target = path.join(__dirname, "remote.socketio.js");
+const link = path.join(getScriptFolder(), "mpvremote", "remote.socketio.js");
 
 // const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf-8'));
 
-console.log([
-  `mpv-remote`,
-  '',
-  'You need to symlink the script file to your mpv scripts folder:',
-  '',
-  `  ${os.platform() === 'win32' ? `mklink "${link}" "${target}"\n  or\n  New-Item -ItemType SymbolicLink -Path "${link}" -Target "${target}"` : `ln -s "${target}" "${link}"`}`,
-  '',
-  ''
-].join('\n'));
+console.log(
+  [
+    `mpv-remote`,
+    "",
+    "First copy mpvremote plugin to your MPV plugins folder:",
+    "",
+    `  ${
+      os.platform() === "win32"
+        ? `copy "${pluginDir}" "${pluginTarget}"`
+        : `cp -r "${pluginDir}" "${pluginTarget}"`
+    }`,
+    "",
+    "You need to symlink the script file to your MPV scripts folder:",
+    "",
+    `  ${
+      os.platform() === "win32"
+        ? `mklink "${link}" "${target}"\n  or\n  New-Item -ItemType SymbolicLink -Path "${link}" -Target "${target}"`
+        : `ln -s "${target}" "${link}"`
+    }`,
+
+    "",
+    `You can edit your port settings here: ${link}`,
+  ].join("\n")
+);
 
 function getScriptFolder() {
   let mpvHome;
 
-  if (os.platform() === 'win32') {
-    mpvHome = process.env['MPV_HOME'] || '%APPDATA%/mpv';
+  if (os.platform() === "win32") {
+    mpvHome = process.env["MPV_HOME"] || "%APPDATA%/mpv";
   } else {
-    mpvHome = process.env['MPV_HOME'];
+    mpvHome = process.env["MPV_HOME"];
     if (!mpvHome) {
-      const xdgConfigHome = process.env['XDG_CONFIG_HOME'] || '$HOME/.config';
-      mpvHome = path.join(xdgConfigHome, 'mpv');
+      const xdgConfigHome = process.env["XDG_CONFIG_HOME"] || "$HOME/.config";
+      mpvHome = path.join(xdgConfigHome, "mpv");
     }
   }
 
-  return path.join(mpvHome, 'scripts');
+  return path.join(mpvHome, "scripts");
 }
